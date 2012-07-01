1b303f4b18439a5e4bb7b99bfc80f845cb870562
Antti Leppä <antti.leppa@foyt.fi>
2012-07-01 19:32:23 +0300

    ** Shared ckc-plugin source into github

---
+++ src/ckc/lang/en.js
@@ -0,0 +1,13 @@
+CKEDITOR.plugins.setLang('ckc', 'en', {
+  ckc : {
+    saveButtonTooltip: "Save",
+    documentCreatedMessage: 'Document was created successfully',
+    couldNotConnectToServer : 'Could not connect to a server',
+    couldNotParseServerResponse: 'Could not parse server response',
+    permissionDenied: 'Permission denied',
+    unknownServerError: 'Unknown server error occured',
+    initializationError: 'Unknown error occured while initializing collaboration mode',
+    conflictWhileApplyingUpdates: 'Version conflict with another co-writer. Document was reverted back to last saved revision.',
+    unsavedDocumentWarning: 'Warning: You are working on an unsaved document.'
+  }
+});
---
+++ src/ckc/lang/fi.js
@@ -0,0 +1,13 @@
+CKEDITOR.plugins.setLang('ckc', 'fi', {
+  ckc : {
+    saveButtonTooltip: "Tallenna",
+    documentCreatedMessage: 'Dokumentti luotiin onnistuneesti',
+    couldNotConnectToServer : 'Palvelimeen ei saatu yhteyttä',
+    couldNotParseServerResponse: 'Palvelimen vastausta ei voitu tulkita',
+    permissionDenied: 'Lupa evätty',
+    unknownServerError: 'Palvelimella tapahtui tuntematon virhe',
+    initializationError: 'Tuntematon virhe tapahtui alustaessa yhteiskirjoitustilaa',
+    conflictWhileApplyingUpdates: 'Versioristiriita toisen yhteiskirjoittajan kanssa. Dokumentti palautettiin edelliseen tallennettuun tilaan',
+    unsavedDocumentWarning: 'Varoitus: Dokumenttia ei ole tallennettu.'
+  }
+});
