b15592ac15c0481e311e3d5d8ea07a675ea715e6
Antti Leppä <antti.leppa@foyt.fi>
2012-07-10 23:24:05 +0300

    ** Changed version into 0.0.4-SNAPSHOT ** Added ckcContentChange and ckcPropertiesChange evenst firing before content and/or properties change ** Added ckcDocumentCreate and ckcDocumentCreated events firing before and after document creation ** Changed document creation request to send content and properties inside post body instead of url (fixes encoding probles, where unicode characters were encoded with uri encoding)

--- src/ckc/plugin.js
+++ src/ckc/plugin.js
@@ -583,29 +583,40 @@
       });
     },
     createDocument: function (editor, callback) {
-      var content = encodeURIComponent(getCurrentContent(editor));
-      var properties = encodeURIComponent(hashSerialize(getCurrentProperties(editor)));
+      var content = getCurrentContent(editor);
+      var properties = getCurrentProperties(editor);
       
-      this._doPost(connectorUrl + "?action=CREATE&content=" + content + '&properties=' + properties, null, function (success, responseJson) {
-        if (!success) {
-          displayMessage('SEVERE', lang.documentCreateError);
-        } else {        
-          switch (responseJson.status) {
-            case "OK":
-              updateSavedContent(content);
-              updateDocumentId(responseJson.documentId);
-              updateSavedRevision(responseJson.revisionNumber);
-              setToken(responseJson.token);
-  
-              if ((typeof callback) == 'function')
-                callback();
-            break;
-            default:
-              displayMessage('SEVERE', lang.unknownServerError);
-            break;
+      if (editor.fire("ckcDocumentCreate", {
+        content: content,
+        properties: properties
+      }) !== true) {
+        var data = "content=" + encodeURIComponent(content) + '&properties=' + encodeURIComponent(hashSerialize(properties));
+        this._doPost(connectorUrl + "?action=CREATE", data, function (success, responseJson) {
+          if (!success) {
+            displayMessage('SEVERE', lang.documentCreateError);
+          } else {        
+            switch (responseJson.status) {
+              case "OK":
+                updateSavedContent(content);
+                updateDocumentId(responseJson.documentId);
+                updateSavedRevision(responseJson.revisionNumber);
+                setToken(responseJson.token);
+                
+                editor.fire("ckcDocumentCreated", {
+                  content: content,
+                  properties: properties
+                });
+    
+                if ((typeof callback) == 'function')
+                  callback();
+              break;
+              default:
+                displayMessage('SEVERE', lang.unknownServerError);
+              break;
+            }
           }
-        }
-      });
+        });
+      }
     },
     
     /**
@@ -733,59 +744,89 @@
         }
         
         if (contentDirty||propertiesDirty) {
-          this._doPost(connectorUrl + '?action=SAVE&documentId=' + id + "&revision=" + getSavedRevision(), data, function (success, responseJson) {
-            if (!success) {
-              displayMessage('SEVERE', responseJson.errorMessage);
-              if ((typeof callback) == 'function') {
-                callback();
+          var cancelled = false;
+          
+          if (contentDirty) {
+            if (editor.fire('ckcContentChange', {
+              source: "local",
+              content: snapshot
+            }) === true) {
+              cancelled = true;
+              updateCurrentContent(editor, getSavedContent());
+            }
+          }
+          
+          if (propertiesDirty) {
+            if (editor.fire('ckcPropertiesChange', {
+              source: "local",
+              properties: properties
+            }) === true) {
+              cancelled = true;
+              var oldProperties = getSavedProperties();
+              for (var propertyName in oldProperties) {
+                updateCurrentProperty(editor, propertyName, oldProperties[propertyName]);
               }
-            } else {    
-              switch (responseJson.status) {
-                case "OK":
-                  if (responseJson.revisionNumber) {
-                    if (contentDirty) {
-                      updateSavedContent(snapshot);
-                      editor.fire('ckcContentChanged', {
-                        source: "local"
-                      });
-                    }
-                    
-                    if (propertiesDirty) {
-                      updateSavedProperties(properties);
-                      
-                      var changedProperties = new Array();
-                      
-                      for (var property in propertiesDiff) {
-                        changedProperties.push({
-                          name: property,
-                          value: propertiesDiff[property]
+            }
+          }
+          
+          if (cancelled == false) {
+            this._doPost(connectorUrl + '?action=SAVE&documentId=' + id + "&revision=" + getSavedRevision(), data, function (success, responseJson) {
+              if (!success) {
+                displayMessage('SEVERE', responseJson.errorMessage);
+                if ((typeof callback) == 'function') {
+                  callback();
+                }
+              } else {    
+                switch (responseJson.status) {
+                  case "OK":
+                    if (responseJson.revisionNumber) {
+                      if (contentDirty) {
+                        updateSavedContent(snapshot);
+                        editor.fire('ckcContentChanged', {
+                          source: "local"
                         });
                       }
                       
-                      editor.fire('ckcPropertiesChanged', {
-                        changedProperties: changedProperties,
-                        source: "local"
-                      });
+                      if (propertiesDirty) {
+                        updateSavedProperties(properties);
+                        
+                        var changedProperties = new Array();
+                        
+                        for (var property in propertiesDiff) {
+                          changedProperties.push({
+                            name: property,
+                            value: propertiesDiff[property]
+                          });
+                        }
+                        
+                        editor.fire('ckcPropertiesChanged', {
+                          changedProperties: changedProperties,
+                          source: "local"
+                        });
+                      }
+                      updateSavedRevision(responseJson.revisionNumber);
                     }
-                    updateSavedRevision(responseJson.revisionNumber);
-                  }
-                break;
-                case "CONFLICT":
-                  displayMessage('WARNING', lang.conflictWhileApplyingUpdates);
-                  revert(editor);
-  //                updateCurrentContent(editor, original);
-                break;
-                default:
-                  displayMessage('SEVERE', lang.unknownServerError);
-                break;
+                  break;
+                  case "CONFLICT":
+                    displayMessage('WARNING', lang.conflictWhileApplyingUpdates);
+                    revert(editor);
+    //                updateCurrentContent(editor, original);
+                  break;
+                  default:
+                    displayMessage('SEVERE', lang.unknownServerError);
+                  break;
+                }
+                
+                editor.resetDirty();
+                
+                if ((typeof callback) == 'function')
+                  callback();
               }
-              
-              editor.resetDirty();
-              
-              if ((typeof callback) == 'function')
-                callback();
-            }
-          });
+            });
+          } else {
+            if ((typeof callback) == 'function')
+              callback();
+          }
         } else {
           if ((typeof callback) == 'function')
             callback();
